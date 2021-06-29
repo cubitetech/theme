9a373530d7da08511dac34ed305f3ce450533f3d
Cubite Technologies <devops@cubite.io>
2021-06-29 00:07:46 +0000

    Lilac Init

---
+++ lms/jstools/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  important: true,
+  future: {
+      removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
+  },
+  purge: {
+      enabled: false, //true for production build
+      content: [
+          '../**/templates/*.html',
+          '../**/templates/**/*.html'
+      ]
+  },
+  theme: {
+      extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
